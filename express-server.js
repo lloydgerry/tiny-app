b34ef88d5bd535662c05cb6bda24149a75b46fe3
Lloyd <lloyd.gerry@gmail.com>
2019-10-03 18:12:16 +0000

    got urls_show.ejs working again and reset variables for that, not all user funcs operational

--- express-server.js
+++ express-server.js
@@ -22,9 +22,7 @@ let newShortUrl = "";
 // FUNCTIONS
 
 const generateRandomString = function(length) {
-
   let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-   
   for (let i = 0; i < length; i++) {
     newShortUrl += possible.charAt(Math.floor(Math.random() * possible.length));
   }
@@ -51,7 +49,7 @@ const foundPassword = function(passwordId) {
     }
   }
   return false;
-}
+};
 
 const getTemplateVars = function(req) {
   let userId = req.cookies['userId'];
@@ -60,36 +58,52 @@ const getTemplateVars = function(req) {
   // console.log("var user: ", user);
   let templateVars = {
     urls: urlDatabase,
+    urlsforUserId: urlsForUser(userId),
     user: user
   };
   // console.log(templateVars);
-  return templateVars
+  return templateVars;
+};
+
+//returns array of object urls based on userId (cookie id)
+const urlsForUser = function(id) {
+  let urlIds = [];
+  // console.log(id);
+  for (let key in urlDatabase) {
+    if (urlDatabase[key].userId === id) {
+      urlIds.push(urlDatabase[key])
+      // console.log("adding this key: ", urlDatabase[key]);
+    }
+    
+  }
+  return urlIds;
 }
 // ===============================
 // SERVER OBJECTS
 //Users object
 const users = {
-  "userrandomId1": {
-    id: "userrandomId1",
+  "5f67f5": {
+    id: "5f67f5",
     email: "user@example.com",
     password: "123-words"
   },
-  "userrandomId2": {
-    id: "user2RandomId2",
+  "d6gd323d": {
+    id: "d6gd323d",
     email: "user2@example.com",
     password: "the-secure-password"
   },
   "test": {
-    id: "userrandomId1",
-    email: "user@example.com",
+    id: "test",
+    email: "test@test.com",
     password: "test"
   },
 };
 
 //Basic Url DB
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {shortURL: "b2xVn2", longURL: "http://www.lighthouselabs.ca", userId: "test"},
+  "9sm5xK": {shortURL: "9sm5xK", longURL: "http://www.google.com", userId: "d6gd323d"},
+  "7f6t7d": {shortURL: "7f6t7d", longURL: "https://abebooks.com", userId: "test"},
 };
 
 // ===============================
@@ -113,17 +127,23 @@ app.get("/login", (req, res) => {
   // console.log(req.body)
   templateVars = getTemplateVars(req);
   res.render("login", templateVars)
-})
+});
 
 //Create new URL
 app.get("/urls/new", (req, res) => {
   let templateVars = getTemplateVars(req);
-  res.render("urls_new", templateVars);
+  let userId = req.cookies["userId"];
+  if (userId === undefined) {
+    res.redirect("/login");
+  } else {
+    res.render("urls_new", templateVars);
+  }
 });
 
 //"index"
 app.get("/urls", (req, res) => {
   let templateVars = getTemplateVars(req);
+  let userId = req.cookies["userId"];
   res.render("urls_index", templateVars);
 });
 
@@ -139,13 +159,9 @@ app.post("/login", (req, res) => {
     res.send("YOU SHALL NOT PASS: This email is not registered.")
   } else if (foundPassword(password) === false) {
       res.status(403);
-      res.send("YOU SHALL NOT PASS: This email or password was incorrect");
-      console.log("We passed to found password")
+      res.send("YOU SHALL NOT PASS: The email or password was incorrect");
 
     } else if (foundPassword(password) === true) {
-      console.log("We got false flag for password")
-      // console.log("users: ", users);
-      // console.log("user: ", userId);
       res.cookie('userId', user["id"]);
       res.redirect('/urls');
     }
@@ -162,7 +178,6 @@ app.post("/logout", (req, res) => {
 app.post("/urls", (req, res) => {
   //change id length as num
   let num = 6;
-
   urlDatabase[generateRandomString(num)] = req.body.longURL;
   res.redirect('/url/' +  newShortUrl);
 });
@@ -175,7 +190,17 @@ app.get("/u/:shortURL", (req, res) => {
 
 //Short URL Page
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let shortURL = req.params.shortURL;
+  let userId = req.cookies['userId'];
+  let user = users[userId];
+  let templateVars = {
+    urls: urlDatabase,
+    shortURL: shortURL,
+    user: user,
+   }
+  // let templateVars = getTemplateVars(req);
+  console.log("shortURL templateVars: ", templateVars)
+  console.log("req: ", req.params.shortURL)
   res.render("urls_show", templateVars);
 });
 
